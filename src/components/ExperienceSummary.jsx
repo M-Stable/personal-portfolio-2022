ec286ebdb6cdf910073bb2b816988dd7c79c7722
M-Stable <fchanghlee@gmail.com>
2021-11-16 18:03:01 +1300

    bug fix attempt 2

--- src/components/ExperienceSummary.jsx
+++ src/components/ExperienceSummary.jsx
@@ -5,6 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 const ExperienceSummary = ({ exp, order }) => {
   const expcard = useRef();
   const [isEven, setEven] = useState(false);
+  const [startRow, setStartRow] = useState("");
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -21,16 +22,15 @@ const ExperienceSummary = ({ exp, order }) => {
 
   useEffect(() => {
     setEven(order % 2 === 0);
+    setStartRow("row-start-" + (order + 1));
   }, [order]);
 
   return (
     <div
       ref={expcard}
-      className={`relative mb-24 bg-secondary p-14 flex flex-col items-center w-full xl:w-3/4 row-start-${
-        order + 1
-      } ${isEven ? "xl:col-start-1" : "xl:col-start-2"} ${
-        isEven && "xl:justify-self-end"
-      }`}
+      className={`relative mb-24 bg-secondary p-14 flex flex-col items-center w-full xl:w-3/4 ${startRow} ${
+        isEven ? "xl:col-start-1" : "xl:col-start-2"
+      } ${isEven && "xl:justify-self-end"}`}
     >
       <div
         className={`absolute invisible xl:visible h-6 w-6 ${
